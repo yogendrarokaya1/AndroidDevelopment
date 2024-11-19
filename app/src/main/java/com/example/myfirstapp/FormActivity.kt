a9cc86fc10cc301c248a0e452ba6599c625397a5
yogendrarokaya1 <52326146+yogendrarokaya1@users.noreply.github.com>
2024-11-19 16:18:55 +0545

    first commit

---
+++ app/src/main/java/com/example/myfirstapp/FormActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FormActivity : AppCompatActivity() {
+    lateinit var editText: EditText
+    lateinit var button: Button
+    lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_form)
+
+        editText = findViewById(R.id.name)
+        button = findViewById(R.id.btnSubmit)
+        textView = findViewById(R.id.display)
+
+        button.setOnClickListener{
+            val inputText = editText.text.toString()
+            textView.text = inputText
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapp/RadioActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RadioButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RadioActivity : AppCompatActivity() {
+    lateinit var apple: RadioButton
+    lateinit var mango: RadioButton
+    lateinit var grapes: RadioButton
+    lateinit var image: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_radio)
+        apple = findViewById(R.id.btnApple)
+        mango = findViewById(R.id.btnMango)
+        grapes = findViewById(R.id.btnGrapes)
+        image = findViewById(R.id.imageView)
+
+        mango.setOnClickListener{
+            image.setImageResource(R.drawable.apple)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
